fdff575dcfe4ddb961224b4c61c7b9616c72921f
Parker <Parker@Parker-PC>
2014-09-23 17:10:45 +0900

    adding branch

--- grigit/src/main/java/kr/re/ec/grigit/ui/MenuBarFrame.java
+++ grigit/src/main/java/kr/re/ec/grigit/ui/MenuBarFrame.java
@@ -8,22 +8,34 @@ public class MenuBarFrame extends JMenuBar{
 	
 	protected JMenuItem mntmOpenRepo;
 	protected JMenuItem mntmHowtoUseCommand;
+	protected JMenuItem mntmBranch;
+	protected JMenuItem mntmMerge;
 	
 	protected JMenu mnFile;
-	protected JMenu mnCommand;
+	protected JMenu mnHelp;
+	protected JMenu mnRepository;
 	
 	public MenuBarFrame() {
 		
 		mnFile = new JMenu("File");
-		mnCommand = new JMenu("Command");
+		mnRepository = new JMenu("Repository");
+		mnHelp = new JMenu("Help");
+		
 		
 		add(mnFile);
-		add(mnCommand);
+		add(mnRepository);
+		add(mnHelp);
 		//clone commit chjeckout branch merge rebase merge
 		mntmOpenRepo = new JMenuItem("Open Repository");
+		mntmBranch = new JMenuItem("Branch");
+		mntmMerge = new JMenuItem("Merge");
 		mntmHowtoUseCommand = new JMenuItem("How to use Command?");
+		
 		mnFile.add(mntmOpenRepo);
-		mnCommand.add(mntmHowtoUseCommand);
+		mnHelp.add(mntmHowtoUseCommand);
+		mnRepository.add(mntmBranch);
+		mnRepository.add(mntmMerge);
+		
 	}
 
 }
